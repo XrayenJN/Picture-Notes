28cab058757beb910ecdca844c3db7197ba20e2e
jordan <jordannathanael0@gmail.com>
2021-04-16 16:37:40 +0700

    n-attempt fixing 504 problem

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require('http-proxy-middleware');
 
-const port = process.env.ATLAS_URI || 5000;
+const port = process.env.PORT || 5000;
 
 module.exports = function(app) {
     app.use(
