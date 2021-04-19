aa186362138d23880f93ba00998e98d7a0b215c8
jordan <jordannathanael0@gmail.com>
2021-04-19 22:20:40 +0700

    add express.static

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('client/public'))
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
